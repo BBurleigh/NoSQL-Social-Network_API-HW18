893e2787aac21720d14d426c43ab6648b1015a87
Brayden <brayburl224@gmail.com>
2022-12-19 16:25:59 -0500

    completed draft of thoughtRoutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,7 +5,24 @@ const {
     getSingleThought,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    deleteReaction
 } = require('../../controllers/thoughtController');
+
+router.route("/")
+.get(getThoughts)
+.post(createThought);
+
+router.route("/:id")
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+router.route("/:thoughtId/reactions")
+.post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId")
+.delete(deleteReaction);
 
 module.exports = router;
