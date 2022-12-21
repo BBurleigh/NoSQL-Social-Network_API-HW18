cfe708505e9512e9f03fcd31d6e190ddb91522bc
Brayden <brayburl224@gmail.com>
2022-12-20 22:24:32 -0500

    drafted out more of userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,22 +13,37 @@ module.exports = {
         .select('-__v')
         .then((user) =>
         !user
-        ? res.status(404).json({ message: 'There is no user with that ID.'})
+        ? res.status(404).json({ message: 'You cannot find this user since this ID does not exist.'})
         : res.json(user)    
     )
     .catch((err) => res.status(500).json(err));
     },
 
     createNewUser(req, res) {
-
+        User.create(req.body)
+        .then((user) => res.json(user))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
     },
 
     updateUser(req, res) {
-
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true}
+        )
+        .then((user) =>
+        !user
+        ? res.status(404).json({ message: 'Cannot update user since this ID does not exist.' })
+        : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
     },
 
     deleteUser(req, res) {
-
+        
     },
 
     addFriend(req, res) {
