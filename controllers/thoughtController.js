4e62e5fcc689d1ca221047a259be1cafaf1a94fc
Brayden <brayburl224@gmail.com>
2022-12-21 08:53:42 -0500

    began working on more of thoughtController, next step is to figure out part of the deleteThought, if a user does not existanymore

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -8,19 +8,51 @@ module.exports = {
     },
 
     getSingleThought(req, res) {
-
+        thoughts.findOne({ _id: req.params.thoughtId })
+        .select('-__v')
+        .then((thought) =>
+        !thought
+        ? res.status(404).json({ message: 'You cannot find this thought since this ID does not exist.' })
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
     },
 
     createThought(req, res) {
-
+        Thought.create(req.body)
+        .then((thought) => res.json(thought))
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
     },
 
     updateThought(req, res) {
-
+        Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $set: req.body },
+            { runValidators: true, new: true}
+        ) 
+        .then((thought) =>
+        !thought
+        ? res.status(404).json({ message: 'Cannot update thought since this ID does not exist.' })
+        : res.json(thought)
+        )
+        .catch((err) => res.status(500).json(err));
     },
 
     deleteThought(req, res) {
-
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+        .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'You cannot delete this thought because this ID does not exist.' })
+          : User.findOneAndUpdate(
+            { thoughts: req.params.thoughtId },
+            { $pull: { thoughts: { req.params.thoughtId } } },
+            { new: true }
+            )
+      )
+      .then((user) => 
     },
 
     addReaction(req, res) {
